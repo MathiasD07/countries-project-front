1ee97b2c18de1256370ca067b21e517e64bb7ecf
Mathias Davin <davmathias78@gmail.com>
2021-12-05 19:15:46 +0100

    fix: remove useless import

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import CountryDetails from "../pages/CountryDetails";
 
 const Card = (props) => {
     const { country } = props;
